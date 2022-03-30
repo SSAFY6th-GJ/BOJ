d35b1fcea8622e0016c023a45dd7147f32550a44
seongbiny <s30ngb1n@gmail.com>
2022-03-30 21:19:22 +0900

    create: 성빈

---
+++ 11497/11497_성빈.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    lst = list(map(int, input().split()))
+    lst.sort(reverse=True)
+    # print(lst)
+
+    maxv = 0
+    for i in range(n-2):
+        maxv = max(maxv, abs(lst[i]-lst[i+2]))
+    last = abs(lst[-1]-lst[-2])
+    maxv = max(maxv, last)
+
+    print(maxv)
+
+'''
+가장 큰 통나무를 가운데를 기준으로 넣고
+그 다음 크기 순대로 양 옆을 채워나가면 된다.
+처음에는 직접 위의 순서대로 리스트를 채우려 했는데 너무 어려워서
+오름차순으로 정렬한 후 2 인덱스 차이 씩 빼주었다.
+'''
