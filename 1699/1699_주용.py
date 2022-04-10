94223682a08cbf99718e73afbd92742d6abd4ce6
Juyong <ht9943@naver.com>
2022-04-10 19:16:34 +0900

    [create] 1699_주용

---
+++ 1699/1699_주용.py
@@ -0,0 +1,17 @@
+# dp 모르겠어유
+N = int(input())
+dp = [0] * (N+1)
+
+# 틀린 코드
+# import math
+# N = int(input())
+# cnt = 0
+# while N > 0:
+#     start = math.trunc(N**(1/2))
+#     N -= start**2
+#     if N < start**2:
+#         if start > 1:
+#             start -= 1
+#
+#     cnt += 1
+# print(cnt)
