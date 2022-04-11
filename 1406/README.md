0419db87d8e84f31663177531d942df9550140bc
송상빈 <ssbin407@naver.com>
2022-04-11 20:46:25 +0900

    create 1406

---
+++ 1406/README.md
@@ -0,0 +1,2 @@
+# [BOJ 1406번: 에디터](https://www.acmicpc.net/problem/1406)
+
--- 21608/21608_상빈.py
+++ 21608/21608_상빈.py
@@ -4,6 +4,7 @@ dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
 
 
+# 해당 자리 주위에 좋아하는 학생이 얼마나 있는지 확인하는 함수
 def aroundlike(y, x, n):
     cnt = 0
     for i in range(4):
@@ -16,6 +17,7 @@ def aroundlike(y, x, n):
     return cnt
 
 
+# 스텝1 : 비어있는 칸 중에서 좋아하는 학생이 인접한 칸에 가장 많은 칸으로 자리를 정한다.
 def step1(n):
 
     newlst = []
@@ -34,6 +36,7 @@ def step1(n):
     return newlst
 
 
+# 해당 자리 주위에 비어있는 자리가 얼마나 있는지 확인하는 함수
 def aroundempty(y, x):
 
     cnt = 0
@@ -48,6 +51,7 @@ def aroundempty(y, x):
     return cnt
 
 
+# 스텝2 : 1을 만족하는 칸이 여러 개이면, 인접한 칸 중에서 비어있는 칸이 가장 많은 칸으로 자리를 정한다.
 def step2(lst):
 
     newlst = []
@@ -68,6 +72,7 @@ def step2(lst):
     return newlst
 
 
+# 스텝3 : 2를 만족하는 칸도 여러 개인 경우에는 행의 번호가 가장 작은 칸으로, 그러한 칸도 여러 개이면 열의 번호가 가장 작은 칸으로 자리를 정한다.
 def step3(lst):
     for i in range(N):
         for j in range(N):
@@ -75,6 +80,7 @@ def step3(lst):
                 return i, j
 
 
+# 학생의 자리 만족도를 구하는 함수
 def calc(y, x):
 
     now = table[y][x]
@@ -94,7 +100,11 @@ def calc(y, x):
 students = [list(map(int, input().split())) for _ in range(N**2)]
 
 table = [[0]*N for _ in range(N)]
-
+'''
+1. 비어있는 칸 중에서 좋아하는 학생이 인접한 칸에 가장 많은 칸으로 자리를 정한다.
+2. 1을 만족하는 칸이 여러 개이면, 인접한 칸 중에서 비어있는 칸이 가장 많은 칸으로 자리를 정한다.
+3. 2를 만족하는 칸도 여러 개인 경우에는 행의 번호가 가장 작은 칸으로, 그러한 칸도 여러 개이면 열의 번호가 가장 작은 칸으로 자리를 정한다.
+'''
 for i in range(N**2):
     step1_list = step1(i)
     if len(step1_list) == 1:
