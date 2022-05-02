e73a0ed0b4fc6ced859f08e3db3a567b48357c43
Juyong <ht9943@naver.com>
2022-05-02 21:54:49 +0900

    [create] 7490

--- 3085/3085-주용.py
+++ 3085/3085-주용.py
@@ -7,6 +7,7 @@ def check(arr):
             if arr[i][j] == arr[i][j+1]:
                 row_cnt += 1
             else:
+                max_row = max(row_cnt, max_row)
                 row_cnt = 1
         max_row = max(row_cnt, max_row)
 
@@ -17,6 +18,7 @@ def check(arr):
             if arr[j][i] == arr[j+1][i]:
                 col_cnt += 1
             else:
+                max_col = max(col_cnt, max_col)
                 col_cnt = 1
         max_col = max(col_cnt, max_col)
     res = max(max_row, max_col)
@@ -28,17 +30,19 @@ ans = 0
 for i in range(N):
     for j in range(N-1):
         # 열 한번 바꾸고 최대개수 체크
-        arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
-        col_tmp = check(arr)
-        ans = max(col_tmp, ans)
-        # 다시 돌려놓기
-        arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
+        if arr[i][j] != arr[i][j+1]:
+            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
+            col_tmp = check(arr)
+            ans = max(col_tmp, ans)
+            # 다시 돌려놓기
+            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
 for i in range(N):
     for j in range(N-1):
         # # 행 한번 바꾸고 최대개수 체크
-        arr[j][i], arr[j+1][i] = arr[j+1][i], arr[j][i]
-        row_tmp = check(arr)
-        ans = max(row_tmp, ans)
-        # 다시 돌려놓기
-        arr[j][i], arr[j+1][i] = arr[j+1][i], arr[j][i]
+        if arr[j][i] != arr[j+1][i]:
+            arr[j][i], arr[j+1][i] = arr[j+1][i], arr[j][i]
+            row_tmp = check(arr)
+            ans = max(row_tmp, ans)
+            # 다시 돌려놓기
+            arr[j][i], arr[j+1][i] = arr[j+1][i], arr[j][i]
 print(ans)
---
+++ 7490/README.md
@@ -0,0 +1,2 @@
+# [BOJ 7490번: 0 만들기](https://www.acmicpc.net/problem/7490)
+
