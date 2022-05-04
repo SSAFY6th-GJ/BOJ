8bbbcedecd3ac21be74c339b7b634b044ab6e815
seongbiny <s30ngb1n@gmail.com>
2022-05-04 20:48:23 +0900

    update: 성빈

---
+++ 14891/14891_성빈.py
@@ -0,0 +1,6 @@
+import collections
+
+arr = []
+for _ in range(4):
+    arr.append(collections.deque(list(input())))
+
--- 9095/9095_성빈.py
+++ 9095/9095_성빈.py
@@ -1,14 +1,26 @@
 import sys
 input = sys.stdin.readline
 
+def answer(num):
+    if num == 1:
+        return 1
+    elif num == 2:
+        return 2
+    elif num == 3:
+        return 4
+    else:
+        return answer(num-1) + answer(num-2) + answer(num-3)
+
 T = int(input())
 for _ in range(T):
     n = int(input())
-    count = 0
+    print(answer(n))
 
-    arr = []
-
-    for i in range(1, 4):
-        while n != 0:
-            n -= i
-            arr.append(i)
+'''
+정수 1 -> 1 -> 1개
+정수 2 -> (1+1), 2 -> 2개
+정수 3 -> (1+1+1), (1+2), (2+1), 3 -> 4개
+정수 4 -> (1+1+1+1), (1+1+2), (1+2+1), (1+3), (2+1+1), (2+2), (3+1) -> 7개
+정수 5 -> 정수 2 + 정수 3 + 정수 4 -> 13개
+...
+'''
