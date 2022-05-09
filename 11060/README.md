988ca63a2ec1bf045d2c22894d4b4f6d29ecaf46
Juyong <ht9943@naver.com>
2022-05-10 00:28:53 +0900

    [create] 11060

---
+++ 11060/README.md
@@ -0,0 +1,2 @@
+# [BOJ 11060번: 점프 점프](https://www.acmicpc.net/problem/11060)
+
--- 9095/9095_주용.py
+++ 9095/9095_주용.py
@@ -1,4 +1,8 @@
-# 1일때 1, 2일때 2, 3일때 4, 4일때 7, 5일때 13
+# 1일때 1
+# 2일때 2(1+1 , 2)
+# 3일때 4(1+1+1+1, 2+2, 1+3, 4)
+# 4일때 7
+# 5일때 13
 TC = int(input())
 
 for tc in range(1, TC+1):
@@ -8,8 +12,8 @@ for tc in range(1, TC+1):
     res[1] = 1
     if n > 1:
         res[2] = 2
-        if n > 2:
-            res[3] = 4
+    if n > 2:
+        res[3] = 4
     if n > 3:
         for i in range(4, n+1):
             res[i] = res[i-1] + res[i-2] + res[i-3]
